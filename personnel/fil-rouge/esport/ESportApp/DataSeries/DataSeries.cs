b8323c7f255e2564baf7960edfe7fe4ff7470ae0
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-09-09 10:58:12 +0200

    feat(fil-rouge): Modification de la classe DataSeries pour retourner un DataPoint
    [20][DONE]

---
+++ personnel/fil-rouge/esport/ESportApp/DataSeries/DataSeries.cs
@@ -0,0 +1,31 @@
+﻿namespace DataSeries {
+    public class DataSeries<T> {
+        private readonly IEnumerable<DataPoint<T>> _data;
+
+        private DataSeries(IEnumerable<DataPoint<T>> data) => _data = data;
+
+        public static DataSeries<T> From(IEnumerable<DataPoint<T>> source) => new DataSeries<T>(source);
+
+        public static DataSeries<T> FromCsv(string filename, Func<string[], DataPoint<T>> parser) {
+            List<DataPoint<T>> data = new List<DataPoint<T>>();
+            try {
+                List<string> content = File.ReadAllLines(filename).ToList();
+                foreach (string line in content.Skip(1)) {
+                    string[] cols = line.Split(',');
+                    data.Add(parser(cols));
+                }
+            } catch (Exception e) {
+                Console.WriteLine($"Erreur d'ouverture du fichier {e.Message}");
+            }
+            return From(data);
+        }
+
+        public int Count => _data.Count();
+        public IEnumerable<T> Values => _data.Select(dp => dp.Value);
+        public IEnumerable<DataPoint<T>> DataPoints => _data;
+
+        //public override string ToString() {
+        //    return $"DataSerie<{typeof(T).Name}>: {Count} points: {Environment.NewLine}{String.Join(Environment.NewLine, _data.Select(s => s).ToArray())}";
+        //}
+    }
+}
--- personnel/fil-rouge/esport/ESportApp/ESportApp/Program.cs
+++ personnel/fil-rouge/esport/ESportApp/ESportApp/Program.cs
@@ -2,22 +2,51 @@
 using DataSeries;
 using ESportApp;
 
-var valorant = DataSerie<ValorantMatch>.FromCsv(@".\data\valorant.csv", ParseValorant);
-var cs2 = DataSerie<Cs2Match>.FromCsv(@".\data\cs2.csv", ParseCS2);
-var lol = DataSerie<LolMatch>.FromCsv(@".\data\Lol.csv", ParseLoL);
+DataSeries<DataPoint<ValorantMatch>> valorant;
+DataSeries<DataPoint<Cs2Match>> cs2;
+DataSeries<DataPoint<LolMatch>> lol;
+
+valorant = DataSeries<DataPoint<ValorantMatch>>.FromCsv(@"data\valorant.csv", ParseValorant);
+cs2 = DataSeries<DataPoint<Cs2Match>>.FromCsv(@"data\cs2.csv", ParseCS2);
+lol = DataSeries<DataPoint<LolMatch>>.FromCsv(@"data\Lol.csv", ParseLoL);
 
 Console.WriteLine($"Il y a  {valorant.Count} matches dans la série Valorant"); // 3
 Console.WriteLine($"Il y a  {cs2.Count} matches dans la série CS2"); // 3
 Console.WriteLine($"Il y a  {lol.Count} matches dans la série LoL"); // 3
 
+var biggame = valorant.Values
+    .Where(vm => vm.Value.Player == "Léa")
+    .Where(vm => vm.Value.Kills >= 20)
+    .Where(vm => vm.Value.Assists >= 5)
+    .OrderBy(vm => vm.Timestamp)
+    .Last();
+Console.WriteLine(biggame.Timestamp.ToString("dd/MM/yyyy") + " : " + biggame.Value.Player + " a fait un gros match avec " + biggame.Value.Kills + " kills et " + biggame.Value.Assists + " assists.");
+
+ValorantMatch dylanthird = valorant.Values
+    .Where(ValorantMatch => ValorantMatch.Value.Player == "Dylan")
+    .ElementAt(3)
+    .Value;
+Console.WriteLine("Dans son 4ème match, Dylan a fait " + dylanthird.Kills + " kills.");
+
+DataSeries<DataPoint<LolMatch>> noeswins = DataSeries<DataPoint<LolMatch>>.From(
+    lol
+    .Values
+    .Where(lolmatch => lolmatch.Value.Player == "Noé" && lolmatch.Value.Won)
+    .ToList()
+    );
+
+Console.WriteLine(noeswins);
+
 Console.ReadKey();
 
-ValorantMatch ParseValorant(string[] cols) {
-    return new ValorantMatch(cols[1], cols[2], int.Parse(cols[3]), int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), cols[8] == "TRUE");
+DataPoint<ValorantMatch> ParseValorant(string[] cols) {
+    ValorantMatch match = new ValorantMatch(cols[1], cols[2], int.Parse(cols[3]), int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), bool.Parse(cols[8]));
+    DateTime date = DateTime.Parse(cols[0]);
+    return new DataPoint<ValorantMatch>(date, match);
 }
-Cs2Match ParseCS2(string[] cols) {
-    return new Cs2Match(cols[1], cols[2], cols[3], int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), cols[8] == "TRUE");
+DataPoint<Cs2Match> ParseCS2(string[] cols) {
+    return new DataPoint<Cs2Match>(DateTime.Parse(cols[0]), new Cs2Match(cols[1], cols[2], cols[3], int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), bool.Parse(cols[8])));
 }
-LolMatch ParseLoL(string[] cols) {
-    return new LolMatch(cols[1], cols[2], int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), int.Parse(cols[8]), cols[9] == "TRUE");
+DataPoint<LolMatch> ParseLoL(string[] cols) {
+    return new DataPoint<LolMatch>(DateTime.Parse(cols[0]), new LolMatch(cols[1], cols[2], int.Parse(cols[4]), int.Parse(cols[5]), int.Parse(cols[6]), int.Parse(cols[7]), int.Parse(cols[8]), bool.Parse(cols[9])));
 }
